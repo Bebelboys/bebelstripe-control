34b7e66482fb4b558b92328367df6dd2901a004c
Florian Reichardt <florian.reichardt@freenet.de>
2020-04-19 14:44:49 +0200

    Refactor Strobo Implementation
    Since the strobo functionality is implemented in the 'LEDWall' class, the 'Strobo' class is no longer required.
    The old class 'Strobo' and bindings in 'main.py' have been deleted.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import LEDWall
 import FFT
-import Strobo
 import SharedVariables
 import threading
 import sys
@@ -12,13 +11,10 @@ def main():
         shared_vars = SharedVariables.SharedVariables()
         ledwall = LEDWall.LEDWall()
         fft = FFT.FFT()
-        # strobo = Strobo.Strobo()
 
         t1 = threading.Thread(target=ledwall.music_spectrum, args=(shared_vars,))
         t1.daemon = True
         t1.start()
-        # strobo_thread = threading.Thread(target=strobo.start, args=(shared_vars,))
-        # strobo_thread.music_spectrum()
         t2 = threading.Thread(target=fft.start, args=(shared_vars,))
         t2.daemon = True
         t2.start()
