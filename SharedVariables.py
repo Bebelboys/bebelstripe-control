bae16e95c6ab004cecdc8a08007559a90c53d015
Florian Reichardt <florian.reichardt@freenet.de>
2020-04-20 15:32:05 +0200

    Update SharedVariables.py
    Added all settings and config variables to class 'SharedVariables'

--- SharedVariables.py
+++ SharedVariables.py
@@ -2,13 +2,19 @@ import numpy as np
 
 
 class SharedVariables:
+    on = True
+    mode = enumerate('music', 'strobo', 'ambient')
     brightness = 1.0
-    musicSpectrumLevels = np.array(8 * [0])
     primaryColor = [248, 24, 148]
-    dot = True
     secondaryColor = [0, 0, 255]
+    musicSpectrumLevels = np.array(8 * [0])
+    fallingDot = True
+    dotSpeed = 1
+    fftWeightings = [4, 4, 8, 16, 32, 32, 64, 128]
     stroboFrequency = 0.05
     stroboDutyCycle = 0.5
+    ambientPulsing = True
+    ambientFrequency = 10.0
 
     def __init__(self):
         pass
